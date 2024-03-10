9e402563d5b6cdf829ebbcc3843d184aec8d6e7a
Thomas REMY <contact.t.remy777@gmail.com>
2024-03-10 17:34:11 +0100

    feat: Add Morse code functionality
    - Added morse.h header file with functions for converting characters and strings to Morse code
    - Implemented Morse code table and conversion functions in morse.c
    - Included functions to play Morse code as beeps on a buzzer

---
+++ transmitter/Core/Inc/morse.h
@@ -0,0 +1,56 @@
+/*
+ * morse.h
+ *
+ *  Created on: Mar 7, 2024
+ *      Author: thomas
+ */
+
+#pragma once
+
+/**
+ * The duration (in ms) of the elementary rythm.
+ */
+#define SHORT_BEEP_LENGTH 100
+
+/**
+ * Converts a single character to it's morse representation.
+ *
+ * @param input The input character to convert.
+ * @return A string representation of the input character in morse.
+ */
+char* ctos(char input);
+
+/**
+ * Converts an ASCII string to it's morse representation.
+ *
+ * @param input The input ASCII string to convert.
+ * @return A string representation of the input ASCII string in morse.
+ */
+char* atom(const char *input);
+
+/**
+ * Plays the corresponding morse rythms on a buzzer.
+ *
+ * @param morse A string containing the morse representation of the text to play.
+ */
+void morse_to_beep(const char *morse);
+
+/**
+ * Plays a short beep (1 ER) and waits 1 ER.
+ */
+void short_beep();
+
+/**
+ * Plays a long beep (3 ER) and waits 1 ER.
+ */
+void long_beep();
+
+/**
+ * Waits 2 ER for inter letter spacing.
+ */
+void inter_letters();
+
+/**
+ * Waits 2 ER for inter word spacing.
+ */
+void inter_words();
